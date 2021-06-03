d820ede4238087fe5a46a8f7b6170c61c55bd710
Omari Sopromadze <coma.spurs@gmail.com>
2021-06-03 14:55:06 +0200

    add MartianRobotApplication as @SpringBootApplication

---
+++ src/main/java/com/guidesmiths/martian_robot/MartianRobotApplication.java
@@ -0,0 +1,14 @@
+package com.guidesmiths.martian_robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianRobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartianRobotApplication.class, args);
+    }
+
+}
+
